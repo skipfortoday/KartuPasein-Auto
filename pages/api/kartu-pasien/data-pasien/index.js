816b48054818ff96e58eb400635ba0560f0c9afd
Rizqi Pratama <rev4rmy@gmail.com>
2021-06-21 16:07:00 +0700

    ADD ADO QUERY / TESTING QUERY

---
+++ pages/api/kartu-pasien/data-pasien/index.js
@@ -0,0 +1,20 @@
+const qryKartuPasien = require("../../../../src/config/sql-kartu-pasien");
+
+export default async function handler(req, res) {
+  try {
+    let querydata = await qryKartuPasien.query(
+      `SELECT NKP, NAMA FROM tblDataPasien`
+    );
+    res.status(200).json({
+      success: true,
+      message: "Berhasil Mendapatkan Data",
+      data: querydata,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+      data: false,
+    });
+  }
+}
