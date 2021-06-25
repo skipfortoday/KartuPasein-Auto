87da07777d9af4de74a9d8e4faa883e9ca2a4548
Rizqi Pratama <rev4rmy@gmail.com>
2021-06-25 17:07:51 +0700

    Master Data  Import Data

---
+++ pages/master/import-data/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Admin from "../../../src/layouts/admin";
+
+const ImportData = () => {
+  return <div></div>;
+};
+
+export default ImportData;
+ImportData.layout = Admin;
---
+++ pages/master/master-data/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Admin from "../../../src/layouts/admin";
+
+MasterData = () => {
+  return <div></div>;
+};
+
+export default MasterData;
+MasterData.Layout = Admin;
