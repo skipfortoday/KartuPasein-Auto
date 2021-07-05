e3507bc4a2b3e9da7e5d8e424df36b25ae6ccbee
Rizqi Pratama <rev4rmy@gmail.com>
2021-07-06 06:42:19 +0700

    commit

--- pages/api/kartu-pasien/lokasi-foto-after/data/index.js
+++ pages/api/kartu-pasien/lokasi-foto-after/data/index.js
@@ -1,18 +1,51 @@
 const qryKartuPasien = require("../../../../../src/config/sql-kartu-pasien");
+import firebase from "../../../../../src/config/firebase";
+import moment from "moment";
 
 export default async function handler(req, res) {
   try {
     if (req.method === "POST") {
       try {
-        let querydata = await qryKartuPasien.query(
-          ` ${req.body.data}
-            ;`
+        let q = `DELETE FROM [KartuPasien_SB02_Test].[dbo].[tmpPerawatanLokasiFotoAfter];
+        INSERT INTO [KartuPasien_SB02_Test].[dbo].[tmpPerawatanLokasiFotoAfter]
+        ("NoAuto", "NoAutoPerawatan", "Keterangan", "UserEntry", "LoginComp", "CompName", "TglActivitas", "JamActivitas", "LokasiFotoAfter", "TglAuto") VALUES ${req.body.data
+          .replace(/\s+/g, " ")
+          .trim()};`;
+        console.log(q);
+        await qryKartuPasien.execute(q);
+        await qryKartuPasien.execute(
+          ` MERGE tblPerawatanLokasiFotoAfter AS Target
+                  USING (SELECT * FROM tmpPerawatanLokasiFotoAfter) AS Source
+                  ON (Target.NoAuto = Source.NoAuto)
+                  WHEN MATCHED THEN
+                      UPDATE SET
+                              Target.NoAuto = Source.NoAuto,
+                              Target.NoAutoPerawatan = Source.NoAutoPerawatan,
+                              Target.Keterangan = Source.Keterangan, 
+                              Target.UserEntry = Source.UserEntry,
+                              Target.LoginComp = Source.LoginComp,
+                              Target.CompName = Source.CompName,
+                              Target.TglActivitas = Source.TglActivitas,
+                              Target.JamActivitas = Source.JamActivitas,
+                              Target.LokasiFotoAfter = Source.LokasiFotoAfter,
+                              Target.TglAuto = Source.TglAuto
+                  WHEN NOT MATCHED BY TARGET THEN
+                       INSERT
+                       ("NoAuto", "NoAutoPerawatan", "Keterangan", "UserEntry", "LoginComp", "CompName", "TglActivitas", "JamActivitas", "LokasiFotoAfter", "TglAuto")
+                        VALUES  (Source.NoAuto, Source.NoAutoPerawatan, Source.Keterangan, Source.UserEntry, Source.LoginComp, Source.CompName, Source.TglActivitas, Source.JamActivitas, Source.LokasiFotoAfter, Source.TglAuto)
+                  OUTPUT $action, Inserted.*, Deleted.*;`
         );
-        console.log(querydata);
-        res.status(200).json({
+        await firebase
+          .database()
+          .ref("/datapasien")
+          .update({
+            sb2: moment.parseZone(moment()).format("YYYY-MM-DD HH:mm:ss"),
+          });
+
+        await res.status(200).json({
           success: true,
           message: "Berhasil Post Data",
-          data: querydata,
+          data: false,
         });
       } catch (error) {
         console.log(error);
@@ -51,6 +84,6 @@ export default async function handler(req, res) {
     }
   } catch (error) {
     console.log(error);
-    res.json(error);
+    res.status(500).json("Eror Sycron dokter ", error);
   }
 }
