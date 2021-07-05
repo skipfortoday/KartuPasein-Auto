dfa921219049cdc5c94993d9d8b9bd54b2fabe57
Rizqi Pratama <rev4rmy@gmail.com>
2021-07-05 12:47:13 +0700

    DatPasien

--- pages/api/kartu-pasien/data-pasien/data/index.js
+++ pages/api/kartu-pasien/data-pasien/data/index.js
@@ -1,27 +1,43 @@
 const qryKartuPasien = require("../../../../../src/config/sql-kartu-pasien");
+import firebase from "../../../../../src/config/firebase";
+import moment from "moment";
 
 export default async function handler(req, res) {
   try {
     if (req.method === "POST") {
       try {
-        let querydata = await qryKartuPasien.query(
-          `INSERT INTO tmpTblDataPasien (
-            NKP,NoAuto,TglAwalDaftar,Nama,Alamat,TelpRumah,
-            HP,Fax,TglLahir,NoDist,NoSponsor,Status,Keterangan,
-            TglActivitas,JamActivitas,UserEntry,LoginComp,CompName,PasienLama,Sponsor,
-            Exported,LastCallDateUltah,tempCallPasien,tempCallDate,
-            tempCallTime,tempCallKet,tempNoAutoHistoryCallPasienUltah,
-            IDSponsor,LokasiFoto,NoKTP,NamaKTP,TempatLahir,AlamatKTP,
-            TelpKTP,Kota,KotaKTP,KotaSMS,StatusLtPack,NoDistLtPack,
-            IDSponsorLtPack,PinBB,StatusDiskonPasien,TglAuto
-            ) VALUES ${req.body.data}
+        let querydata = await qryKartuPasien.execute(
+          `
+          DELETE FROM "dbo"."tmpBA";
+          INSERT INTO "tmpBA"
+           ("IDBA", "NamaBA", "Status", "Exported" , TglAuto) VALUES ${req.body.data}
             ;`
         );
-        console.log(querydata);
+
+        let mergedata = await qryKartuPasien.execute(`
+        MERGE tblBA AS Target
+        USING (SELECT * FROM tmpBA) AS Source
+        ON (Target.IDBA = Source.IDBA)
+        WHEN MATCHED THEN
+            UPDATE SET Target.NamaBA = Source.NamaBA, 
+                    Target.Status = Source.Status,
+                Target.Exported = Source.Exported, 
+                Target.TglAuto = Source.TglAuto 		  
+        WHEN NOT MATCHED BY TARGET THEN
+            INSERT (IDBA,NamaBA,Status,Exported,TglAuto)
+            VALUES (Source.IDBA, Source.NamaBA, Source.Status,
+        Source.Exported,Source.TglAuto)
+        OUTPUT $action, Inserted.*, Deleted.*;`);
+        firebase
+          .database()
+          .ref("/datapasien")
+          .update({
+            sb2: moment.parseZone(moment()).format("YYYY-MM-DD HH:mm:ss"),
+          });
         res.status(200).json({
           success: true,
-          message: "Berhasil Mendapatkan Data",
-          data: querydata,
+          message: "Berhasil Post Data",
+          data: mergedata,
         });
       } catch (error) {
         console.log(error);
@@ -34,8 +50,7 @@ export default async function handler(req, res) {
     } else if (req.method === "GET") {
       try {
         let querydata = await qryKartuPasien.query(
-          `SELECT NKP, Nama , Alamat, CONVERT(varchar, TglAuto,113) as TglAuto 
-        FROM tblDataPasien ORDER BY TglAuto DESC`
+          `SELECT NKP, Nama , Alamat, CONVERT(varchar, TglAuto,113) as WaktuSyc FROM tblDataPasien ORDER BY TglAuto DESC`
         );
         res.status(200).json({
           success: true,
@@ -58,6 +73,70 @@ export default async function handler(req, res) {
     }
   } catch (error) {
     console.log(error);
-    res.json(error);
+    res.json("Eror Sycron dokter ", error);
   }
 }
+
+// const qryKartuPasien = require("../../../../../src/config/sql-kartu-pasien");
+
+// export default async function handler(req, res) {
+//   try {
+//     if (req.method === "POST") {
+//       try {
+//         let querydata = await qryKartuPasien.query(
+//           `INSERT INTO tmpTblDataPasien (
+//             NKP,NoAuto,TglAwalDaftar,Nama,Alamat,TelpRumah,
+//             HP,Fax,TglLahir,NoDist,NoSponsor,Status,Keterangan,
+//             TglActivitas,JamActivitas,UserEntry,LoginComp,CompName,PasienLama,Sponsor,
+//             Exported,LastCallDateUltah,tempCallPasien,tempCallDate,
+//             tempCallTime,tempCallKet,tempNoAutoHistoryCallPasienUltah,
+//             IDSponsor,LokasiFoto,NoKTP,NamaKTP,TempatLahir,AlamatKTP,
+//             TelpKTP,Kota,KotaKTP,KotaSMS,StatusLtPack,NoDistLtPack,
+//             IDSponsorLtPack,PinBB,StatusDiskonPasien,TglAuto
+//             ) VALUES ${req.body.data}
+//             ;`
+//         );
+//         console.log(querydata);
+//         res.status(200).json({
+//           success: true,
+//           message: "Berhasil Mendapatkan Data",
+//           data: querydata,
+//         });
+//       } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//           success: false,
+//           message: error,
+//           data: false,
+//         });
+//       }
+//     } else if (req.method === "GET") {
+//       try {
+//         let querydata = await qryKartuPasien.query(
+//           `SELECT NKP, Nama , Alamat, CONVERT(varchar, TglAuto,113) as TglAuto
+//         FROM tblDataPasien ORDER BY TglAuto DESC`
+//         );
+//         res.status(200).json({
+//           success: true,
+//           message: "Berhasil Mendapatkan Data",
+//           data: querydata,
+//         });
+//       } catch (error) {
+//         res.status(500).json({
+//           success: false,
+//           message: error,
+//           data: false,
+//         });
+//       }
+//     } else {
+//       res.status(404).json({
+//         success: false,
+//         message: "Anda Kehilangan Arah",
+//         data: false,
+//       });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.json(error);
+//   }
+// }
--- pages/api/kartu-pasien/lokasi-foto-before/data/index.js
+++ pages/api/kartu-pasien/lokasi-foto-before/data/index.js
@@ -6,33 +6,28 @@ export default async function handler(req, res) {
   try {
     if (req.method === "POST") {
       try {
-        console.log(req.body.data);
-        const gen = await qryKartuPasien.execute(
-          `
-          DELETE FROM "dbo"."tmpPerawatanLokasiFotoBefore";
-          INSERT INTO "tmpPerawatanLokasiFotoBefore"
-           ("NoAuto", "NoAutoPerawatan", "Keterangan", "UserEntry" ,"LoginComp","CompName",
-           "TglActivitas","JamActivitas","LokasiFotoBefore",TglAuto) VALUES ${req.body.data}
-            ;`
+        await qryKartuPasien.execute(
+          `DELETE FROM "dbo"."tmpPerawatanLokasiFotoBefore";
+          INSERT INTO "tmpPerawatanLokasiFotoBefore" ("NoAuto", "NoAutoPerawatan", "Keterangan", "UserEntry" ,"LoginComp","CompName","TglActivitas","JamActivitas","LokasiFotoBefore","TglAuto") VALUES ${req.body.data};`
         );
         let mergedata = await qryKartuPasien.execute(`
         MERGE tblPerawatanLokasiFotoBefore AS Target
         USING (SELECT * FROM tmpPerawatanLokasiFotoBefore) AS Source
         ON (Target.NoAuto = Source.NoAuto)
         WHEN MATCHED THEN
-            UPDATE SET Target.NoAutoPerawatan = Source.NoAutoPerawatan, 
+            UPDATE SET Target.NoAutoPerawatan = Source.NoAutoPerawatan,
                 Target.Keterangan = Source.Keterangan,
-                Target.UserEntry = Source.UserEntry, 
-                Target.LoginComp = Source.LoginComp, 
-                Target.CompName = Source.CompName, 
-                Target.TglActivitas = Source.TglActivitas, 
-                Target.JamActivitas = Source.JamActivitas, 
-                Target.LokasiFotoBefore = Source.LokasiFotoBefore, 
-                Target.TglAuto = Source.TglAuto 		  
+                Target.UserEntry = Source.UserEntry,
+                Target.LoginComp = Source.LoginComp,
+                Target.CompName = Source.CompName,
+                Target.TglActivitas = Source.TglActivitas,
+                Target.JamActivitas = Source.JamActivitas,
+                Target.LokasiFotoBefore = Source.LokasiFotoBefore,
+                Target.TglAuto = Source.TglAuto
         WHEN NOT MATCHED BY TARGET THEN
             INSERT (NoAuto, NoAutoPerawatan,Keterangan, UserEntry ,LoginComp,CompName,
             TglActivitas,JamActivitas,LokasiFotoBefore,TglAuto)
-            VALUES (Source.NoAuto, Source.NoAutoPerawatan,Source.Keterangan, 
+            VALUES (Source.NoAuto, Source.NoAutoPerawatan,Source.Keterangan,
               Source.UserEntry ,Source.LoginComp,Source.CompName,
               Source.TglActivitas,Source.JamActivitas,Source.LokasiFotoBefore,Source.TglAuto)
         OUTPUT $action, Inserted.*, Deleted.*;`);
