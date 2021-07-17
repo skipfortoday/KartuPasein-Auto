774fbbc31f80f26833bc46a3a98ac09afea3342c
Rizqi Pratama <rev4rmy@gmail.com>
2021-07-17 21:49:27 +0700

    data pasien fix unuserd var

--- pages/api/kartu-pasien/data-pasien/data/index.js
+++ pages/api/kartu-pasien/data-pasien/data/index.js
@@ -195,7 +195,7 @@ export default async function handler(req, res) {
         res.status(200).json({
           success: true,
           message: "Berhasil Post Data",
-          data: mergedata,
+          data: req.body.data,
         });
       } catch (error) {
         console.log(error);
